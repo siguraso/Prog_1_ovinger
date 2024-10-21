f994318021b20ab404066b171d9c27cf2f5f6828
sigurdandris <sigurd.andris.sorengen@gmail.com>
2024-10-21 17:55:54 +0200

    started work on the student class

---
+++ oving_9/src/Student.java
@@ -0,0 +1,33 @@
+public class Student {
+    private final String name;
+    private int antOppg;
+
+    /**
+     * Student ID.
+     * @param name
+     * The student's name.
+     * @param antOppg
+     * The amount of tasks that the student has completed as well as approved.
+     */
+    Student(String name, int antOppg) {
+        this.name = name;
+        this.antOppg = antOppg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAntOppg() {
+        return antOppg;
+    }
+
+    public void okAntOppg(int okning) {
+        this.antOppg += okning;
+    }
+
+    public String toString() {
+        return this.getName() + ", Godkjent: " + this.getAntOppg() + "oppgaver.";
+    }
+
+}
